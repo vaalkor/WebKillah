d0167a429a9aa70feb82c1d96977c3738e899305
Robert Stamper <robertstamper39@gmail.com>
2019-04-14 05:54:34 +0100

    Cleared up a bit. changed default key combo

--- content_script.js
+++ content_script.js
@@ -5,7 +5,8 @@ var killer_enabled = false;
 document.addEventListener('mousemove', onMouseMove);
 document.addEventListener('mousedown', onMouseDown);
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse)=>{
+chrome.runtime.onMessage.addListener((message, sender, sendResponse)=>
+{
     if(message.action===CLICK_KILLER)
     {
         killer_enabled = !killer_enabled;
@@ -16,30 +17,23 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse)=>{
 
 function onMouseDown(e)
 {
-    if(killer_enabled)
+    if(killer_enabled && currentElement != null)
     {
-        if(currentElement)
-        {
-            currentElement.remove();
-            currentElement = null;
-        }
+        currentElement.remove();
+        currentElement = null;
     }
 }
 function onMouseMove(e)
 {
-    if(killer_enabled)
+    if(!killer_enabled) return;
+
+    var x = e.clientX;
+    var y = e.clientY;
+    var elem = document.elementFromPoint(x,y);
+    if(elem && currentElement !== elem)
     {
-        var x = e.clientX;
-        var y = e.clientY;
-        var elem = document.elementFromPoint(x,y);
-        if(elem && currentElement !== elem)
-        {
-            //console.log(elem.outerHTML);
-            if(currentElement) currentElement.classList.remove("delete-highlight");
-            currentElement = elem;
-            currentElement.classList.add("delete-highlight");
-    
-        }
+        if(currentElement) currentElement.classList.remove("delete-highlight");
+        currentElement = elem;
+        currentElement.classList.add("delete-highlight");
     }
-    
 }
--- event_script.js
+++ event_script.js
@@ -3,11 +3,7 @@ var click_killer_enabled = false;
 
 chrome.commands.onCommand.addListener(function(command) {
         click_killer_enabled = !click_killer_enabled;
-        console.log("Enable click killer pressed!!");
         chrome.tabs.query({active: true}, function(tabs){
             chrome.tabs.sendMessage(tabs[0].id, {action: CLICK_KILLER}, function(response) {});  
         });
-        console.log(command);
-        console.log(command == CLICK_KILLER);
-        console.log(command === CLICK_KILLER);
 });
